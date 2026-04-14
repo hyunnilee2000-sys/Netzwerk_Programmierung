4f68051accc6f65ce3abb4c4926508682686f4d0
Lee <hlee@fe.hhi.de>
2026-04-14 16:49:46 +0200

    Vollständiger Projekt-Upload

---
+++ website.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shikgoo Settings Test</title>
+</head>
+<body>
+
+    <h1>JavaScript HTML DOM</h1>
+    <h2>The getElementById() Method</h2>
+
+    <p id="demo">Hier stand vorher etwas anderes.</p>
+
+    <script src="script.js"></script>
+
+</body>
+</html>
